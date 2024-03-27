9d5e75104147facbd1fbb8440a97c334137e2c08
Debbie30 <cherotichdebby11@gmail.com>
2024-03-27 13:49:01 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    if (typeof customerName === 'string') {
+        customerName = customerName.toUpperCase();
+    } else {
+        console.error('customerName is not a string.');
+    }
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'bob'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not bob'
+}
